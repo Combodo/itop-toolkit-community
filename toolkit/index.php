1f8e3466d6d8f4b47b3124882d8f9d2054b74b40
Denis Flaven <denis.flaven@combodo.com>
2013-07-31 16:45:16 +0000

    Ugly fix for a nasty change in jQuery UI behavior: UI tabs were considered as "Ajax" tabs when the page has a "base" tag, which was not the case in previous versions. Cf http://bugs.jqueryui.com/ticket/8637
    SVN:trunk[180]
    
    Former-commit-id: c921d06331ddc63b0d12a8c3a8705ca96d97aca3

--- toolkit/index.php
+++ toolkit/index.php
@@ -359,7 +359,18 @@ EOF
 	// Tabs, using JQuery BBQ to store the history
 	// The "tab widgets" to handle.
 	var tabs = $('#tabbedContent');
-	    
+
+	// Ugly patch for a change in the behavior of jQuery UI:
+	// Before jQuery UI 1.9, tabs were always considered as "local" (opposed to Ajax)
+	// when their href was beginning by #. Starting with 1.9, a <base> tag in the page
+	// is taken into account and causes "local" tabs to be considered as Ajax
+	// unless their URL is equal to the URL of the page...
+	$('#tabbedContent ul li a').each(function() {
+		var sHash = location.hash;
+		var sCleanLocation = location.href.toString().replace(sHash, '').replace(/#$/, '');
+    	$(this).attr("href", sCleanLocation+$(this).attr("href"));
+	});
+			    
 	// This selector will be reused when selecting actual tab widget A elements.
 	var tab_a_selector = 'ul.ui-tabs-nav a';
 	  
